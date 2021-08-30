f4421a9f0b1c137a7d33eb66cba38a2b7aa50fc3
Utkarsh Gumphekar <54374420+utkarsh-cmc@users.noreply.github.com>
2021-08-30 22:03:58 +0530

    Add files via upload

---
+++ list_tuples_prbm1.py
@@ -0,0 +1,11 @@
+# Fruit enterd by user to be displayed in the list
+f1=input("enter your fruit 1\n ")
+f2=input("enter your fruit 2\n ")
+f3=input("enter your fruit 3\n ")
+f4=input("enter your fruit 4\n ")
+f5=input("enter your fruit 5\n ")
+f6=input("enter your fruit 6\n ")
+f7=input("enter your fruit 7\n ")
+
+myfruitslist=[f1,f2,f3,f4,f5,f6,f7]
+print(myfruitslist)
